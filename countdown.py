1c42dbc053fdf0cebe2fd4aba844a0309f309aae
Blender Newbie <50071334+BlenderNewbie2020@users.noreply.github.com>
2024-08-13 14:32:56 +0100

    Update countdown.py infix_dict
    Return the infix_dict with tuples as keys.
    
    Remove console tests.

--- countdown.py
+++ countdown.py
@@ -1,12 +1,9 @@
+"""
+Python script to attempt to solve the Channel 4 Countdown numbers game - Dictionary method
+"""
+
 from itertools import chain, combinations_with_replacement, permutations, zip_longest
-
-'''
-# comment out the original code and keep for reference
-
-# Define the two lists
-numbers = [75, 5, 9, 3, 8, 10]
-operators = ['+', '-', '*', '/']
-target = 699
+import re
 
 def daisy(l):
     k = ([[i for i in j if i]
@@ -17,57 +14,28 @@ def daisy(l):
         return chain.from_iterable([[(i[0], i[1]), i[2]], [i[0], (i[1], i[2])]]
                                    for i in k if len(i)==3)
     return k
+
+numbers = [75, 5, 9, 3, 8, 10]
+numbers = ['75', '5', '9', '3', '8', '10']
+operators = ['+', '-', '*', '/']
+target = 699
+
+combis = [c for i in range(2, len(numbers)+1) for c in permutations(numbers, i)]
+symbol = [p for r in range(2,7) for p in combinations_with_replacement(operators, r-1)]
+
+# use the walrus operator to create the partition key and result value on the fly
+# only if the value is an integer and positive, saving the key as a tuple for
+# processing later
+infix_dict = {tuple(re.split(r'([\+\-\*\/])', xy)):z
+              for x in combis
+              for y in symbol
+              if len(x)==len(y)+1
+              if (z:=eval(xy:=''.join(chain(*(zip_longest(x, y, fillvalue=''))))))%1==0
+              if z>0
+              }
 '''
-
-'''
-# Use the python console to make a dictionary
->>> numbers = [75, 5, 9, 3, 8, 10]
-
-# make the numbers strings because converting Python types is a pain
->>> numbers = ['75', '5', '9']
->>> operators = ['+', '-', '*', '/']
->>> target = 699
-
-# make lists of the permutations and operators for testing
->>> combis = [c for i in range(2, len(numbers)+1) for c in permutations(numbers, i)]
->>> symbol = [p for r in range(2,7) for p in combinations_with_replacement(operators, r-1)]
-
-# verify the permutations tuples
->>> test_digits = {c:c for c in combis}
->>> test_digits
-    {('75', '5'): ('75', '5'), ('75', '9'): ('75', '9'), ('5', '75'): ('5', '75'), ('5', '9'): ('5', '9'),... }
-
-# try making a dictionary of numbers and operators
->>> test_tups = {x:(x, y) for x in combis for y in symbol if len(x)==len(y)+1}
->>> test_tups
-    {('75', '5'): (('75', '5'), ('/',)), ('75', '9'): (('75', '9'), ('/',)), ... }
-
-# zip the numbers and operators to infix notation
->>> test_zipl = {x:zip_longest(x, y, fillvalue='') for x in combis for y in symbol if len(x)==len(y)+1}
->>> test_zipl
-    {('75', '5'): [('75', '/'), ('5', '')], ('75', '9'): [('75', '/'), ('9', '')], ...
-
-# try to evaluate a chain
->>> q=(('75', '/'), ('5', ''))
->>> eval(''.join(chain(*q)))
-    15.0
-
-# make the key more meaningful
->>> test_eval = {x+y:eval(''.join(chain(*(zip_longest(x, y, fillvalue=''))))) for x in combis for y in symbol if len(x)==len(y)+1}
->>> test_eval
-    {('75', '5', '+'): 80, ('75', '5', '-'): 70, ('75', '5', '*'): 375, ...}
-
-# use the walrus operator to create the key and value on the fly
-# only if the value is an integer and positive
->>> test_dict = {xy:z
-        for x in combis
-        for y in symbol
-        if len(x)==len(y)+1
-        if (z:=eval(xy:''.join(chain(*(zip_longest(x, y, fillvalue=''))))))%1==0
-        if z>0}
->>> test_dict # look for no decimals or negative numbers
-    {'75+5': 80, '75-5': 70, '75*5': 375, '75/5': 15.0, '75+9': 84, ...}
-
+>>> infix_dict
+    {('75', '+', '5'): 80, ('75', '-', '5'): 70, ('75', '*', '5'): 375, ('75', '/', '5'): 15.0, ...}
 '''
 
 '''
@@ -98,3 +66,4 @@ zipped_product = (''.join(
 result = (i for i in zipped_product if eval(i) == target)
 
 print(list(result))
+'''
